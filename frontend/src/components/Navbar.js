c20fcaf24679ad5b11abd504d986e48ce7a86517
katirum <kartikaprw@gmail.com>
2022-12-14 22:44:40 +0100

    attempting to create & style start page

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,8 +1,8 @@
 import React from "react";
 /* import styled, { css } from "styled-components/macro"; */
 /* import { BrowserRouter as Router/* , Switch, Route */ 
-import { NavWrapper, Logo } from "utils/GlobalStyles";
-import LogoImg from '../utils/assets/logoimage.png'
+/* import { NavWrapper, Logo } from "utils/GlobalStyles";
+import LogoImg from '../utils/assets/logoimage.png'; */
 import { HamburgerMenu } from "./HamburgerMenu";
 
 export const Navbar = () => {
