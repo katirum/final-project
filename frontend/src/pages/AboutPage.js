3d05ce53a1ae49d022e85a48415de97751dc06c0
katirum <kartikaprw@gmail.com>
2022-12-30 18:11:45 +0100

    created contect for contact page and about page

--- frontend/src/pages/AboutPage.js
+++ frontend/src/pages/AboutPage.js
@@ -1,15 +1,36 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import { MainScreen } from 'components/MainScreen'
+import { Button, InnerWrapper } from 'utils/GlobalStyles'
+import styled from 'styled-components/macro'
 
 export const AboutPage = () => {
     return(
-        
-                <MainScreen title='About' >
+        <AboutPageContainer >
+            <InnerWrapper>
 
-                    <h1 children='Does the about work?'/>
+            <MainScreen title='Languable' >
+                    <p>Our background is the inspiration behind the idea of this project. We have been in places where our native language is not the language of the place we live(d) in. Sometimes it can be hard to practice our skills without getting judged. We created this website for people who want to learn languages with no judgement, prejudice and most importantly, while having tasty cakes and warm cup of tea or coffee. If you are interested to know more, please join us!</p> 
+                    <Link to="/login"><Button
+                type="button">Start</Button></Link>
                 </MainScreen>
-                    
-              
+                </InnerWrapper>
+                </AboutPageContainer >
                 )
-
 }
+
+const AboutPageContainer = styled.div`
+
+    display: flex;
+    text-align: center;
+    margin: 20%;
+    color: var(--secondary);
+    border: 2px solid var(--secondary);
+border-radius: 15px;
+padding: 50px 0;
+
+p{
+    text-align: justify;
+    margin: 5% 0;
+}
+`
--- frontend/src/pages/ContactPage.js
+++ frontend/src/pages/ContactPage.js
@@ -1,15 +1,96 @@
 import React from 'react'
-import { MainScreen } from 'components/MainScreen'
-
+import { InnerWrapper, Container } from 'utils/GlobalStyles'
+import creators from '../utils/creators.json'
+import mailimg from '../utils/assets/email.png'
+import githubimg from '../utils/assets/github.png'
+import portfolioimg from '../utils/assets/web.png'
+import styled from 'styled-components/macro'
 export const ContactPage = () => {
     return(
-        
-                <MainScreen title="Contact" >
-
+        <Container>
+            <InnerWrapper>
+                <h2>Hello, Creators!</h2>
+                    <p>Get in touch with our creators to know more about this project or our other projects</p>
+                    <ContactPageContainer>
+                        {creators.map((item) => (
+                    <CreatorContainer key={item.id}>
+           <a
+              href={item.portfolio}
+              target="_blank"
+              rel="noreferrer"
+              role="button">
+                <Img src={item.image} alt="profpic" />
+            </a>
+            <CreatorInfoContainer><h3>{item.name}</h3>
+            
+            <IconContainer><a
+            href={`mailto: ${item.email}`}
+            target="_blank"
+            rel="noreferrer"
+            role="button"><IconImg src={mailimg} alt='email'/>
+            </a>
+            <a
+            href={item.github}
+            target="_blank"
+            rel="noreferrer"
+            role="button"><IconImg src={githubimg} alt='github'/>
+            </a>
+            <a
+            href={item.portfolio}
+            target="_blank"
+            rel="noreferrer"
+            role="button"><IconImg src={portfolioimg} alt='portfolio'/></a></IconContainer>
+            
+            </CreatorInfoContainer>
+            </CreatorContainer>
+         
+        ))}</ContactPageContainer>
                     
-                </MainScreen>
-                    
-              
+                </InnerWrapper>  </Container> 
                 )
-
 }
+
+const ContactPageContainer = styled.div`
+    display: grid;
+  grid-template-columns: 100%;
+  margin-top: 5%;
+  align-items: center;
+  color: var(--secondary);
+  @media (min-width: 668px){
+    grid-template-columns: 50% 50%;
+    
+  }
+         
+`
+const Img = styled.img`
+    border-radius: 50%;
+    width: 300px;
+    height: 300px;
+    padding: 5px;
+    object-fit: cover;
+    border: 3px solid var(--secondary);
+`
+const IconImg = styled.img`
+    width: 30px;
+    height: 30px;
+    filter: invert(94%) sepia(100%) saturate(150%) hue-rotate(240deg) brightness(111%) contrast(92%);
+    margin-right: 10px;
+`
+
+const IconContainer = styled.div`
+display:flex;
+`
+
+const CreatorContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
+
+const CreatorInfoContainer = styled.div`
+display: flex;
+flex-direction: column;
+align-items: center;
+margin: 20px 0;
+`
