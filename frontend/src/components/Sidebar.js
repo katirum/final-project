702e865486a90f782bcd6262e5a17bf098d2f8be
katirum <kartikaprw@gmail.com>
2023-01-02 14:54:50 +0100

    merging conflicts

---
+++ frontend/src/components/Sidebar.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export const Sidebar = () => {
+return(
+    <SidebarContainer>
+        {/* basically cthe dashboard */}
+        <p>My Profile</p>
+        <p>Create Event</p>
+        {/* Shows created events and where you can edit events */}
+        <p>My Events</p>
+        <p>Guidelines</p>
+    </SidebarContainer>
+)
+}
+
+const SidebarContainer = styled.div`
+    position: relative;
+    left: 0;
+    background-color: var(--secondary);
+    color: var(--primary);
+    text-decoration: underline;
+    line-height: 2;
+    height: 100vh;
+    position: fixed;
+    padding: 30px;
+    
+`
